dc72e96e78172e5c5bb800b032520e0810362903
asdf1899 <araid.anas99@gmail.com>
2021-10-26 20:28:14 +0000

    Deploying to gh-pages from @ asdf1899/asdf1899.github.io@d2914478c112801c77285d8e59a9fb030c4c4584 🚀

---
+++ _next/static/rcrud1NGnzGbrE6LFln-G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/458-e065b5e185ffdcd524b9.js","static/chunks/pages/index-b49de21825170f7c2408.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
