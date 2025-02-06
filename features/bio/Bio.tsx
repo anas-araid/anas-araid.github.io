e8f73b524c821cb68044d277cf46bea8a25f5351
anas <araid.anas99@gmail.com>
2025-02-06 19:01:08 +0100

    chore: updated copy

--- features/bio/Bio.tsx
+++ features/bio/Bio.tsx
@@ -8,7 +8,7 @@ const Bio = (): JSX.Element => {
   return (
     <div className='mt-20'>
       <p className='text-xl mt-8 font-font-normal'>
-        Well, I’m a non-award winning frontend developer, Human-Computer Interaction graduate and maker of dad jokes without being a dad.
+        Well, I’m a non-award winning frontend developer, hci graduate and maker of dad jokes without being a dad.
       </p>
       <p className='text-xl mt-2 font-normal'>
         Lately I’m interested in spatial interfaces, design systems and micro interactions.
@@ -17,7 +17,7 @@ const Bio = (): JSX.Element => {
       <p className='text-xl mt-4 font-normal'>
         I was born and raised in Trento, a small city in the heart of the Italian Alps.
       </p>
-      <p className='text-xl mt-2 font-normal'>Outside of work I enjoy cycling, firefighting and spending time with friends.</p>
+      <p className='text-xl mt-2 font-normal'>Outside of work I enjoy cycling, firefighting and spending quality time with friends.</p>
       <h2 className='text-2xl mt-8 font-medium '>And that’s it.</h2>
       <p className='text-xl mt-4 mb-32 font-normal'>
         The best way to contact me is on{' '}
