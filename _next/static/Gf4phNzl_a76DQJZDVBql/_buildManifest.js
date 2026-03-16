f0852abce8c38a5474e37424adfb59d76955893e
anas-araid <araid.anas99@gmail.com>
2026-03-16 17:47:46 +0000

    Deploying to gh-pages from @ anas-araid/anas-araid.github.io@bcfc3c6f67ef07517b3e9f995268d4ac27d7f022 🚀

--- _next/static/Gf4phNzl_a76DQJZDVBql/_buildManifest.js
+++ _next/static/Gf4phNzl_a76DQJZDVBql/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/03c83cbd5df4fdbc.js"
+    "static/chunks/c8f1c219d81836e9.js"
   ],
   "/404": [
     "static/chunks/a9b69ff9a9cca830.js"
@@ -9,7 +9,7 @@ self.__BUILD_MANIFEST = {
     "static/chunks/e87fb5f7f56cd909.js"
   ],
   "/about": [
-    "static/chunks/a3be432f65dfa845.js"
+    "static/chunks/669bca784a8e2b56.js"
   ],
   "/links": [
     "static/chunks/6cd6a4dd0ba456eb.js"
--- _next/static/chunks/c8f1c219d81836e9.js
+++ _next/static/chunks/c8f1c219d81836e9.js
@@ -1,7 +1,7 @@
 __turbopack_load_page_chunks__("/", [
-  "static/chunks/b350205a47e80115.js",
+  "static/chunks/43ef3f5292ffd542.js",
   "static/chunks/5621b6880af03bf6.js",
   "static/chunks/03cbea769ec72539.js",
   "static/chunks/204e2614e99eb52b.js",
-  "static/chunks/turbopack-6e719f102ad62f9e.js"
+  "static/chunks/turbopack-61f905b398e579ab.js"
 ])
